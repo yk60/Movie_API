4b5d7bee7ad4d34bcd6be70250fe0c3dfbea10a7
yk60 <youkyoung546@gmail.com>
2025-07-02 23:49:30 -0700

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
-    return (
-        <div>
-        </div>
-    );
-
+  return (
+    <div className="container">
+      <div className="item cell1">Cell 1</div>
+      <div className="item cell2">Cell 2</div>
+      <div className="item cell3">Cell 3</div>
+      <div className="item cell4">Cell 4</div>
+      <div className="item content">Main content</div>
+      <div className="item cell5">Cell 5</div>
+    </div>
+  );
 }
 export default Home;

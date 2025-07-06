598692baaba6057ec6f4369d9479a2826c8f02ee
yk60 <youkyoung546@gmail.com>
2025-07-06 15:38:25 -0700

    Write script to fill missing fields in user documents

---
+++ backend/scripts/addMissingFields.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const { User } = require("../models/User");
+
+// matches documents with missing fields, then sets the value of a field to a default value
+async function addMissingFields() {
+  await mongoose.connect(process.env.MONGO_URI);
+
+  await User.updateMany(
+    { watched_movies: { $exists: false } },
+    { $set: { watched_movies: [] } }
+  );
+
+  await User.updateMany(
+    { watchlist: { $exists: false } },
+    { $set: { watchlist: [] } }
+  );
+
+  await User.updateMany(
+    { reviews: { $exists: false } },
+    { $set: { reviews: [] } }
+  );
+
+  console.log("Migration complete.");
+  await mongoose.disconnect();
+}
+
+addMissingFields();
